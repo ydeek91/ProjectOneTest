297f0522283174f0191354b4cdee3619955325b3
Yazan Deek <ydeek91@yahoo.com>
2020-09-18 12:11:46 -0400

    project one test.

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    console.log("I'm ready..");
+    var edamamId = "9f4f3487"
+    var edamamKey = "8e2d662eda8d8f1307f21875dbd2373b"
+
+
+
+
+    $("#meal-search-button").on("click", mealSearchResults);
+    function mealSearchResults(event) {
+
+        event.preventDefault();
+        mealSearchInput = $("#mealSearchInput").val();
+        //console.log(mealSearchInput);
+        getMealRecipe(mealSearchInput);
+    }
+
+
+
+
+
+    // checkMealRecipe function has been given the users search criteria..
+    // ..by the searchResults function, and it will now call on the edamam api..
+
+    function getMealRecipe() {
+        // var mealSearchInput = "korean tacos";
+        var queryURL = `https://api.edamam.com/search?q=${mealSearchInput}&app_id=${edamamId}&app_key=${edamamKey}`
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (data) {
+                console.log(data);
+            });
+    };
+
+
+
+
+
+
+    // checkMealRecipe function has been given the users search criteria..
+    // ..by the searchResults function, and it will now call on the cocktailDB api ..
+    // a random cocktail selection will be provided along side of the users generated recipe.
+    //random option below.
+    var queryURL = `https://www.thecocktaildb.com/api/json/v1/1/random.php`
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (data) {
+            console.log(data);
+        });
+});
